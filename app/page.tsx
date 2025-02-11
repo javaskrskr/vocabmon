a5e753bd928a95173b2de81b3db15955900dfb19
wonderwall-tim <timothy.chow@wonderwall-g.com>
2025-02-11 23:09:26 +0900

    test telegram

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 import { useEffect, useState } from "react";
-import WebApp from '@twa-dev/sdk'
+import { init, useSignal, backButton, initData } from '@telegram-apps/sdk-react'
+
 
 interface UserData {
   id: number;
@@ -12,19 +13,34 @@ interface UserData {
   is_premium?: boolean;
 }
 export default function Home() {
+  init()
+
   const [userData, setUserData] = useState<UserData | null>(null);
+  const isVisible = useSignal(backButton.isVisible);
 
   useEffect(() => {
-    if (WebApp.initDataUnsafe.user) {
-      setUserData(WebApp.initDataUnsafe.user as UserData)
+    console.log('The button is', isVisible ? 'visible' : 'invisible');
+  }, [isVisible]);
+
+  useEffect(() => {
+    backButton.show();
+    return () => {
+      backButton.hide();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (initData) {
+      setUserData(initData.user() as UserData)
     }
-  }, [])
+  }, [initData])
+
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      {userData ? (
+      {userData ?
         <>Hello {userData.username}, welcome to Vocamon</>
-      ) : <div>loading...</div>}
+        : <div>loading...</div>}
     </div>
   )
 }
